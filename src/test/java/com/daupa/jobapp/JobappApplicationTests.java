fc92c99ce771668d93283fe3658791b27c2540cc
acupoflie <127441535+acupoflie@users.noreply.github.com>
2024-10-03 04:23:28 +0400

    first commit

---
+++ src/test/java/com/daupa/jobapp/JobappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daupa.jobapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
